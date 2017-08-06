56f568906ef89f7d37d7f5c8a5040d47970f2bdd
sid-laptop <siddharth2395@gmail.com>
2017-08-06 12:30:08 +0530

    data types

---
+++ php/basic/data_types.php
@@ -0,0 +1,12 @@
+<?php
+$int_var=10;
+$double_var=10.5;
+$str_var="some string";
+$bool_var=true;
+$null_var=null;
+echo "int_var = $int_var <br />
+	double_var = $double_var<br />
+	str_var = $str_var<br />
+	bool_var = $bool_var<br />
+	null_var = $null_var<br />";
+?>
